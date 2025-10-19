bc511952b67b06e2bf638ab52a316d3ef3b4f033
Nikita Ryanov <ryanov.nikita@gmail.com>
2025-10-19 21:43:37 +0300

    refactor: update inmemory iceberg catalog tests

--- modules/catalog/catalog-iceberg-inmemory/src/test/java/kasanari/catalog/iceberg/inmemory/InMemoryIcebergCatalogTest.java
+++ modules/catalog/catalog-iceberg-inmemory/src/test/java/kasanari/catalog/iceberg/inmemory/InMemoryIcebergCatalogTest.java
@@ -2,14 +2,42 @@ package kasanari.catalog.iceberg.inmemory;
 
 
 import kasanari.catalog.iceberg.core.IcebergCatalogAdapter;
-import kasanari.catalog.iceberg.core.DeprecatedIcebergCatalogAdapterTest;
+import kasanari.catalog.iceberg.core.IcebergCatalogAdapterTest;
+import org.apache.iceberg.catalog.Namespace;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class InMemoryIcebergCatalogTest extends DeprecatedIcebergCatalogAdapterTest {
+public class InMemoryIcebergCatalogTest extends IcebergCatalogAdapterTest {
+    private IcebergCatalogAdapter adapter;
+    private final AtomicInteger namespaceId = new AtomicInteger(1);
+
     @Override
     public IcebergCatalogAdapter setupCatalog() {
         var factory = new InMemoryIcebergCatalogFactory();
-        return factory.create(Map.of());
+        this.adapter = factory.create(Map.of());
+        return adapter;
+    }
+
+    @Override
+    public String entityLocation(String name) {
+        return "memory";
+    }
+
+    @Override
+    public String tableName() {
+        return "table";
+    }
+
+    @Override
+    public String viewName() {
+        return "view";
+    }
+
+    @Override
+    public Namespace namespaceName() {
+        var ns = Namespace.of("ns_" + namespaceId.getAndIncrement());
+        adapter.createNamespace(ns);
+        return ns;
     }
 }
