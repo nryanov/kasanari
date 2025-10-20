135957a69e0923d20d65af5b94ce1106c252b72c
Nikita Ryanov <ryanov.nikita@gmail.com>
2025-10-20 22:18:33 +0300

    feat: run configuration for local development

--- modules/server/src/main/java/kasanari/server/configuration/IcebergCatalogConfiguration.java
+++ modules/server/src/main/java/kasanari/server/configuration/IcebergCatalogConfiguration.java
@@ -1,9 +1,18 @@
 package kasanari.server.configuration;
 
+import io.smallrye.config.ConfigMapping;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 
-@Dependent
-public class IcebergCatalogConfiguration {
+import java.util.Map;
 
+@ConfigMapping(prefix = "catalog.iceberg")
+public interface IcebergCatalogConfiguration {
+    IcebergCatalogType type();
+
+    Map<String, String> properties();
+
+    enum IcebergCatalogType {
+        IN_MEMORY, HADOOP, HIVE, JDBC, NESSIE, REST, KASANARI
+    }
 }
