c8f2938741fb020f189fda618303df58ba27c82b
Nikita Ryanov <ryanov.nikita@gmail.com>
2025-10-21 04:34:14 +0300

    update

--- modules/server/src/main/java/kasanari/server/iceberg/IcebergCatalogFactory.java
+++ modules/server/src/main/java/kasanari/server/iceberg/IcebergCatalogFactory.java
@@ -3,13 +3,12 @@ package kasanari.server.iceberg;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Produces;
 import kasanari.catalog.iceberg.core.IcebergCatalogAdapter;
-//import kasanari.catalog.iceberg.hadoop.HadoopIcebergCatalogFactory;
-//import kasanari.catalog.iceberg.hive.HiveIcebergCatalogFactory;
-//import kasanari.catalog.iceberg.inmemory.InMemoryIcebergCatalogFactory;
-//import kasanari.catalog.iceberg.jdbc.JdbcIcebergCatalogFactory;
-//import kasanari.catalog.iceberg.kasanari.KasanariIcebergCatalogFactory;
-//import kasanari.catalog.iceberg.nessie.NessieIcebergCatalogFactory;
-//import kasanari.catalog.iceberg.rest.RestIcebergCatalogFactory;
+import kasanari.catalog.iceberg.hadoop.HadoopIcebergCatalogFactory;
+import kasanari.catalog.iceberg.inmemory.InMemoryIcebergCatalogFactory;
+import kasanari.catalog.iceberg.jdbc.JdbcIcebergCatalogFactory;
+import kasanari.catalog.iceberg.kasanari.KasanariIcebergCatalogFactory;
+import kasanari.catalog.iceberg.nessie.NessieIcebergCatalogFactory;
+import kasanari.catalog.iceberg.rest.RestIcebergCatalogFactory;
 import kasanari.server.configuration.IcebergCatalogConfiguration;
 import org.jboss.logging.Logger;
 
@@ -20,16 +19,14 @@ public class IcebergCatalogFactory {
     @Produces
     public IcebergCatalogAdapter icebergCatalogAdapter(IcebergCatalogConfiguration configuration) {
         logger.infof("Chosen iceberg catalog type is %s", configuration.type());
-//        return switch (configuration.type()) {
-//            case HIVE -> new HiveIcebergCatalogFactory().create(configuration.properties());
-//            case JDBC -> new JdbcIcebergCatalogFactory().create(configuration.properties());
-//            case REST -> new RestIcebergCatalogFactory().create(configuration.properties());
-//            case HADOOP -> new HadoopIcebergCatalogFactory().create(configuration.properties());
-//            case NESSIE -> new NessieIcebergCatalogFactory().create(configuration.properties());
-//            case KASANARI -> new KasanariIcebergCatalogFactory().create(configuration.properties());
-//            case IN_MEMORY -> new InMemoryIcebergCatalogFactory().create(configuration.properties());
-//        };
-
-        return null;
+        return switch (configuration.type()) {
+            case HIVE -> throw new IllegalArgumentException("Hive catalog not supported yet");
+            case JDBC -> new JdbcIcebergCatalogFactory().create(configuration.properties());
+            case REST -> new RestIcebergCatalogFactory().create(configuration.properties());
+            case HADOOP -> new HadoopIcebergCatalogFactory().create(configuration.properties());
+            case NESSIE -> new NessieIcebergCatalogFactory().create(configuration.properties());
+            case KASANARI -> new KasanariIcebergCatalogFactory().create(configuration.properties());
+            case IN_MEMORY -> new InMemoryIcebergCatalogFactory().create(configuration.properties());
+        };
     }
 }
