4e26fc649d0f181628ff7c507fc1fcd6b014c57d
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-05-01 16:56:58 +0300

    update

--- modules/api/api-paimon/build.gradle.kts
+++ modules/api/api-paimon/build.gradle.kts
@@ -8,7 +8,11 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.paimon.api)
+    api(libs.paimon.api)
+
+    implementation(libs.jakarta.annotation.api)
+    implementation(libs.jakarta.inject.api)
+    implementation(libs.jakarta.ws.rs.api)
 }
 
 val rootDir = rootProject.layout.projectDirectory
@@ -17,6 +21,57 @@ val paimonSpecDir = specsDir.dir("paimon")
 val templatesDir = paimonSpecDir.dir("server-templates")
 val generatedDir = layout.buildDirectory.dir("generated")
 val generatedOpenApiSrcDir = layout.buildDirectory.dir("generated/src/main/java")
+val paimonOpenApiMappings = mutableMapOf(
+    "AlterDatabaseRequest" to "org.apache.paimon.rest.requests.AlterDatabaseRequest",
+    "AlterDatabaseResponse" to "org.apache.paimon.rest.responses.AlterDatabaseResponse",
+    "AlterFunctionRequest" to "org.apache.paimon.rest.requests.AlterFunctionRequest",
+    "AlterTableRequest" to "org.apache.paimon.rest.requests.AlterTableRequest",
+    "AlterViewRequest" to "org.apache.paimon.rest.requests.AlterViewRequest",
+    "AuthTableQueryRequest" to "org.apache.paimon.rest.requests.AuthTableQueryRequest",
+    "AuthTableQueryResponse" to "org.apache.paimon.rest.responses.AuthTableQueryResponse",
+    "CommitTableRequest" to "org.apache.paimon.rest.requests.CommitTableRequest",
+    "CommitTableResponse" to "org.apache.paimon.rest.responses.CommitTableResponse",
+    "ConfigResponse" to "org.apache.paimon.rest.responses.ConfigResponse",
+    "CreateBranchRequest" to "org.apache.paimon.rest.requests.CreateBranchRequest",
+    "CreateDatabaseRequest" to "org.apache.paimon.rest.requests.CreateDatabaseRequest",
+    "CreateFunctionRequest" to "org.apache.paimon.rest.requests.CreateFunctionRequest",
+    "CreateTableRequest" to "org.apache.paimon.rest.requests.CreateTableRequest",
+    "CreateTagRequest" to "org.apache.paimon.rest.requests.CreateTagRequest",
+    "CreateViewRequest" to "org.apache.paimon.rest.requests.CreateViewRequest",
+    "ErrorResponse" to "org.apache.paimon.rest.responses.ErrorResponse",
+    "ForwardBranchRequest" to "org.apache.paimon.rest.requests.ForwardBranchRequest",
+    "GetDatabaseResponse" to "org.apache.paimon.rest.responses.GetDatabaseResponse",
+    "GetFunctionResponse" to "org.apache.paimon.rest.responses.GetFunctionResponse",
+    "GetTableResponse" to "org.apache.paimon.rest.responses.GetTableResponse",
+    "GetTableSnapshotResponse" to "org.apache.paimon.rest.responses.GetTableSnapshotResponse",
+    "GetTableDataTokenResponse" to "org.apache.paimon.rest.responses.GetTableTokenResponse",
+    "GetTagResponse" to "org.apache.paimon.rest.responses.GetTagResponse",
+    "GetVersionSnapshotResponse" to "org.apache.paimon.rest.responses.GetVersionSnapshotResponse",
+    "GetViewResponse" to "org.apache.paimon.rest.responses.GetViewResponse",
+    "ListBranchesResponse" to "org.apache.paimon.rest.responses.ListBranchesResponse",
+    "ListConsumersResponse" to "org.apache.paimon.rest.responses.ListConsumersResponse",
+    "ListDatabasesResponse" to "org.apache.paimon.rest.responses.ListDatabasesResponse",
+    "ListFunctionDetailsResponse" to "org.apache.paimon.rest.responses.ListFunctionDetailsResponse",
+    "ListFunctionsGloballyResponse" to "org.apache.paimon.rest.responses.ListFunctionsGloballyResponse",
+    "ListFunctionsResponse" to "org.apache.paimon.rest.responses.ListFunctionsResponse",
+    "ListPartitionsByNamesRequest" to "org.apache.paimon.rest.requests.ListPartitionsByNamesRequest",
+    "ListPartitionsResponse" to "org.apache.paimon.rest.responses.ListPartitionsResponse",
+    "ListSnapshotsResponse" to "org.apache.paimon.rest.responses.ListSnapshotsResponse",
+    "ListTableDetailsResponse" to "org.apache.paimon.rest.responses.ListTableDetailsResponse",
+    "ListTablesGloballyResponse" to "org.apache.paimon.rest.responses.ListTablesGloballyResponse",
+    "ListTablesResponse" to "org.apache.paimon.rest.responses.ListTablesResponse",
+    "ListTagsResponse" to "org.apache.paimon.rest.responses.ListTagsResponse",
+    "ListViewDetailsResponse" to "org.apache.paimon.rest.responses.ListViewDetailsResponse",
+    "ListViewsGloballyResponse" to "org.apache.paimon.rest.responses.ListViewsGloballyResponse",
+    "ListViewsResponse" to "org.apache.paimon.rest.responses.ListViewsResponse",
+    "MarkDonePartitionsRequest" to "org.apache.paimon.rest.requests.MarkDonePartitionsRequest",
+    "RegisterTableRequest" to "org.apache.paimon.rest.requests.RegisterTableRequest",
+    "RenameBranchRequest" to "org.apache.paimon.rest.requests.RenameBranchRequest",
+    "RenameTableRequest" to "org.apache.paimon.rest.requests.RenameTableRequest",
+    "ResetConsumerRequest" to "org.apache.paimon.rest.requests.ResetConsumerRequest",
+    "RollbackSchemaRequest" to "org.apache.paimon.rest.requests.RollbackSchemaRequest",
+    "RollbackTableRequest" to "org.apache.paimon.rest.requests.RollbackTableRequest",
+)
 
 // https://github.com/OpenAPITools/openapi-generator/blob/master/docs/generators/jaxrs-resteasy.md
 // https://github.com/OpenAPITools/openapi-generator/tree/master/modules/openapi-generator-gradle-plugin
@@ -49,8 +104,8 @@ openApiGenerate {
     additionalProperties.put("apiPackage", "kasanari.catalog.paimon.api")
     additionalProperties.put("dateLibrary", "java8")
 
-    modelNameMappings = mutableMapOf()
-    importMappings = mutableMapOf()
+    modelNameMappings = paimonOpenApiMappings.toMutableMap()
+    importMappings = paimonOpenApiMappings.toMutableMap()
 }
 
 the<SourceSetContainer>().named("main") {
@@ -64,7 +119,6 @@ tasks.named<GenerateTask>("openApiGenerate") {
     }
 }
 
-// TODO: fix generation for paimon
-// tasks.named("compileJava") {
-//     dependsOn(tasks.named("openApiGenerate"))
-// }
+ tasks.named("compileJava") {
+     dependsOn(tasks.named("openApiGenerate"))
+ }
---
+++ modules/server/src/main/java/kasanari/server/paimon/PaimonCatalogService.java
@@ -0,0 +1,10 @@
+package kasanari.server.paimon;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.apache.paimon.rest.requests.CreateTableRequest;
+import org.apache.paimon.rest.responses.ErrorResponse;
+
+@ApplicationScoped
+public class PaimonCatalogService {
+}
