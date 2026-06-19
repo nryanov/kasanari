5332e9b64f648f65f7c584cb38ad1788df048784
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-06-19 23:59:18 +0300

    update

--- modules/authorization/authorization-casbin/build.gradle.kts
+++ modules/authorization/authorization-casbin/build.gradle.kts
@@ -9,7 +9,5 @@ dependencies {
     implementation(project(":modules:repository:repository-jdbc"))
     api(project(":modules:repository:repository-management:repository-management-security:repository-management-security-postgres"))
 
-    testImplementation(platform(libs.junit.bom))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation(testFixtures(project(":modules:fixtures:fixtures-common")))
 }
