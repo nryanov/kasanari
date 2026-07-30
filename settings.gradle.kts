e990a9fc22c5ad010107db8b11098c9f9626bf73
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-07-30 23:46:36 +0300

    chore: bump iceberg, paimon & quarkus versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         mavenLocal()
     }
     plugins {
-        id("io.quarkus") version "3.31.2"
+        id("io.quarkus") version "3.37.1"
         id("org.kordamp.gradle.jandex") version "2.3.0"
     }
 }
